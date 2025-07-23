f9cc85a81a70a911503746668e61f3a1587f2938
MarcioInfinity <marcio.ceoinfinity@gmail.com>
2025-07-22 21:40:55 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
-import { AuthProvider } from "./hooks/useAuth";
+import { AuthProvider } from "./hooks/useAuth.tsx";
 import { AuthGuard } from "./components/auth/AuthGuard";
 import { Layout } from "./components/Layout";
 import { Dashboard } from "./components/Dashboard";
